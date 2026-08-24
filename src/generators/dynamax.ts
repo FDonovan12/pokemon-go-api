0a6eb9e3ecc4582945591eb53fb6fe5fb1d8b6d5
FDonovan12 <donovanferreira12@gmail.com>
2026-08-24 12:15:48 +0200

    add imaeg and fix temp evolution evolutionIds

--- src/generators/dynamax.ts
+++ src/generators/dynamax.ts
@@ -1,9 +1,9 @@
-import { PokemonSetting } from '#generated/data/api/intermediate.type.js';
+import { CrushClaw, PokemonSetting } from '#generated/data/api/intermediate.type.js';
 import { IntermediateData } from '#generated/intermediate.index.js';
 import { RawGameMaster } from '#generated/raw.index.js';
 import { FileGenerator } from '../type/fileGenerator.js';
+import { pokeApiClient } from '../utils/pokeApiClient.js';
 
-const DYNAMAX_NOT_RELEASED = ['GYARADOS'];
 const GIGAMAX_NOT_RELEASED = [
     'MELMETAL',
     'CORVIKNIGHT',
@@ -38,12 +38,23 @@ export default class PokemonSettingGenerator extends FileGenerator {
             ]);
 
         const resolveSettings = (pokemonId: string): PokemonSetting[] => {
-            const setting = pokemonSettings.find((s) => s.base.pokemonId === pokemonId)!;
+            const setting = pokemonSettings.find((s) => s.base.pokemonId === pokemonId);
             return pokemonId === 'URSHIFU'
-                ? (setting?.different as PokemonSetting[])
+                ? ((setting?.different as PokemonSetting[]) ?? [])
                 : setting
                   ? [setting]
                   : [];
+        };
+
+        const getIdGigamax = async (dexNumber: number) => {
+            const data = await pokeApiClient.fetchPokemonSpecies(dexNumber);
+            return (
+                +data.varieties
+                    .filter((variety: any) => variety.pokemon.name.includes('gmax'))[0]
+                    ?.pokemon.url.split('/')
+                    ?.filter(Boolean)
+                    ?.last() || dexNumber
+            );
         };
 
         const idsWithBreadMode = (mode: string) =>
@@ -77,41 +88,64 @@ export default class PokemonSettingGenerator extends FileGenerator {
             ],
         ];
 
-        const toOutput = (
+        const NORMAL_MOVE_POWER = 250;
+        const isGigamaxMove = (dynamaxMove: CrushClaw[]) =>
+            (dynamaxMove[0]?.powerLevels?.[0] ?? NORMAL_MOVE_POWER) !== NORMAL_MOVE_POWER;
+        const toOutput = async (
             pokemon: PokemonSetting,
-            dynamaxMove: unknown[],
+            dynamaxMove: CrushClaw[],
             isReleased?: boolean,
-        ) => ({
-            pokemonId: pokemon.base.pokemonId,
-            name: pokemon.base.name,
-            slug: pokemon.base.slug,
-            stats: pokemon.base.stats,
-            quickMoves: quickMoves(pokemon).toObject((move) => move.movementId),
-            dynamaxMove,
-            familyId: pokemon.base.family,
-            ...(isReleased !== undefined && { isReleased }),
-        });
+        ) => {
+            const imageId = isGigamaxMove(dynamaxMove)
+                ? await getIdGigamax(pokemon.base.dexNumber)
+                : pokemon.base.dexNumber;
+            return {
+                pokemonId: pokemon.base.pokemonId,
+                name: pokemon.base.name,
+                dexNumber: pokemon.base.dexNumber,
+                image: `https://cdn.jsdelivr.net/gh/PokeAPI/sprites@master/sprites/pokemon/other/official-artwork/${imageId}.png`,
+                imageShiny: `https://cdn.jsdelivr.net/gh/PokeAPI/sprites@master/sprites/pokemon/other/official-artwork/shiny/${imageId}.png`,
+                slug: pokemon.base.slug,
+                stats: pokemon.base.stats,
+                quickMoves: quickMoves(pokemon).toObject((move) => move.movementId),
+                dynamaxMove,
+                familyId: pokemon.base.family,
+                ...(isReleased !== undefined && { isReleased }),
+            };
+        };
 
         // Dynamax : formes sans évolution
-        const dynamax = idsWithBreadMode('BREAD_MODE')
-            .flatMap(resolveSettings)
-            .filter((pokemon) => pokemon.base.evolutionIds.length === 0)
-            .map((pokemon) => toOutput(pokemon, dynamaxMoveByType(pokemon)));
+        const dynamax = Promise.all(
+            idsWithBreadMode('BREAD_MODE')
+                .flatMap(resolveSettings)
+                .filter((pokemon) => pokemon.base.evolutionIds.length === 0)
+                .map((pokemon) => toOutput(pokemon, dynamaxMoveByType(pokemon))),
+        );
 
         const gigamaxIds = idsWithBreadMode('BREAD_DOUGH_MODE');
 
-        // Gigamax non sortis (liste fixe + formes spéciales)
-        const gigamaxNotReleased = GIGAMAX_NOT_RELEASED.flatMap(resolveSettings).map((pokemon) =>
-            toOutput(pokemon, dynamaxMoveByMapping(pokemon), false),
+        // Gigamax non sortis (liste fixe)
+        const gigamaxNotReleased = Promise.all(
+            GIGAMAX_NOT_RELEASED.flatMap(resolveSettings).map((pokemon) =>
+                toOutput(pokemon, dynamaxMoveByMapping(pokemon), false),
+            ),
         );
 
         // Gigamax sortis
-        const gigamaxReleased = gigamaxIds
-            .filter((id) => id !== 'URSHIFU') // TODO: retirer quand sorti
-            .flatMap(resolveSettings)
-            .concat(otherForms)
-            .map((pokemon) => toOutput(pokemon, dynamaxMoveByMapping(pokemon), true));
+        const gigamaxReleased = Promise.all(
+            gigamaxIds
+                .filter((id) => id !== 'URSHIFU') // TODO: retirer quand sorti
+                .flatMap(resolveSettings)
+                .concat(otherForms)
+                .map((pokemon) => toOutput(pokemon, dynamaxMoveByMapping(pokemon), true)),
+        );
 
-        return dynamax.concat(gigamaxReleased, gigamaxNotReleased);
+        const [dynamaxResult, gigamaxReleasedResult, gigamaxNotReleasedResult] = await Promise.all([
+            dynamax,
+            gigamaxReleased,
+            gigamaxNotReleased,
+        ]);
+
+        return dynamaxResult.concat(gigamaxReleasedResult, gigamaxNotReleasedResult);
     }
 }
