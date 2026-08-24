0e27dee05cf94998c63d737e4473c94cb1288570
FDonovan12 <FDonovan12@users.noreply.github.com>
2026-08-24 21:51:29 +0000

    deploy: dab57e9c4ea22ae013a10fa85eac86878edf4674

--- api/types.ts
+++ api/types.ts
@@ -473,7 +473,7 @@ export interface Dynamax {
     type:        Type[];
     stats:       Stats;
     quickMoves:  { [key: string]: AcidSpray };
-    dynamaxMove: CrushClaw[];
+    dynamaxMove: (CrushClaw | null)[];
     familyId:    string;
     isReleased?: boolean;
 }
